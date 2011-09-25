26d0c1af56cb0b35c16a8c57d710951c55ffe1dc
Matheus da Rosa <matheus.engcomp@gmail.com>
2011-09-25 20:46:07 -0300

    Changing input and adding output

--- fuzzy_input.py
+++ fuzzy_input.py
@@ -1,4 +1,6 @@
 #Input functions for degrees of truth
+#Matheus da Rosa
+#15/09/2011
 
 def input_small(val):
     if val<100:
@@ -10,6 +12,17 @@ def input_small(val):
     return ret
 
 def input_medium(val):
+    if val<100:
+        ret = 0
+    elif val>=100 and val<=200:
+        ret = (val-100)/100.0
+    elif val>=200 and val<=300:
+        ret = 1-((val-200)/100.0)
+    elif val>300:
+        ret = 0
+    return ret
+    
+def input_big(val):
     if val<200:
         ret = 0
     elif val>=200 and val<=300:
@@ -20,28 +33,17 @@ def input_medium(val):
         ret = 0
     return ret
     
-def input_big(val):
-    if val<400:
-        ret = 0
-    elif val>=400 and val<=500:
-        ret = (val-400)/100.0
-    elif val>=500 and val<=600:
-        ret = 1-((val-500)/100.0)
-    elif val>600:
-        ret = 0
-    return ret
-    
 def input_verybig(val):
-    if val<600:
+    if val<300:
         ret = 0
-    elif val>=600 and val<=800:
-        ret = ((val-600)/100.0)
-    elif val>800:
+    elif val>=300 and val<=400:
+        ret = ((val-300)/100.0)
+    elif val>400:
         ret = 1
     return ret
 
 if __name__ == "__main__":
-    val = 350
+    val = 270
     print 'small = %s' %(input_small(val))
     print 'medium = %s' %(input_medium(val))
     print 'big = %s' %(input_big(val))
---
+++ fuzzy_output.py
@@ -0,0 +1,9 @@
+#Input functions for degrees of truth
+#Matheus da Rosa
+#22/09/2011
+
+def discover_centroid(output):
+  for i in range(120):
+    if output[0] != 0:
+      ret[i] = output[0]
+    
